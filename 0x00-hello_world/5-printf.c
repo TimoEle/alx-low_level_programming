8d90b28b93a54948c9bd691748490b8e6e84714e
TimoEle <tinjokuria@gmail.com>
2023-06-15 13:20:02 +0300

    C source code file prints a message using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using printf to output a message
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
