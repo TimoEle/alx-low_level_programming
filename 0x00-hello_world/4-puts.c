dc3810b59cada5b17f6aef046556d360cb242b91
TimoEle <tinjokuria@gmail.com>
2023-06-15 13:14:23 +0300

    C source code file prints a message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints out a message
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char message[] = "Programming is like building a multilingual puzzle";
+
+	puts(message);
+	return (0);
+}
