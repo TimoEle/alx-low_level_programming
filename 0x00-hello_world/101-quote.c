aaef44b8d2fe170d942e431ea1f73656df6ff277
TimoEle <tinjokuria@gmail.com>
2023-06-15 14:27:18 +0300

    Using write function in C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using wite to output a message
+ *
+ * Return: 1 on success
+ */
+int main(void)
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
