b9ef4ae5ac2048c9f1d6b47ad3bde5156d7b21a8
TimoEle <tinjokuria@gmail.com>
2023-06-16 15:36:48 +0300

    Source code that prints all base 10 single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
