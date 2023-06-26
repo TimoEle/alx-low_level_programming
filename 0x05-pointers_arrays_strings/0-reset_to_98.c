2eccb8f41b880089cde5dbffbc0197befac38b21
TimoEle <tinjokuria@gmail.com>
2023-06-26 21:45:34 +0300

    Function that updates the value through dereferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include  "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: Updates the value of an int parameter to 98
+ *@n: parameter
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
