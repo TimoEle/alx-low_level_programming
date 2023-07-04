d9af3219a0b30a6de6bbd96d94a9fa5fc663ba76
TimoEle <tinjokuria@gmail.com>
2023-07-04 12:16:43 +0300

    Function that sets the value of a pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ *
+ * Description: Sets the value of a pointer to a char
+ * @s: double pointer
+ * @to: pointer
+ *
+ * Return: Void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -22,4 +22,6 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 
 void print_diagsums(int *a, int size);
+
+void set_string(char **s, char *to);
 #endif /*MAIN_H*/
