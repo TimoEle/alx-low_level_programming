3055ace5ee1463a1c0c6bdb830a1aaa1a79297c4
TimoEle <tinjokuria@gmail.com>
2023-07-05 22:24:05 +0300

    Functions that checks if an integer is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * prime_recursive- Entry point
+ *
+ * Description: Checks if an integer is a prime number
+ * @n: integer
+ * @divisor: integer
+ *
+ * Return: 1 if it's a prime number else 0
+ */
+
+int prime_recursive(int n, int divisor)
+{
+	
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (divisor == 1)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+
+	return (prime_recursive(n, divisor - 1));
+}
+
+/**
+ * is_prime_number - Entry point
+ *
+ * Description: Calls the prime checker
+ * @n: integer
+ *
+ * Return: function call
+ */
+
+int is_prime_number(int n)
+{
+	return (prime_recursive(n, n - 1));
+}
