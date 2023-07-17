ac308eaeac821b862e1ca9810fc6fcbb562f7912
TimoEle <tinjokuria@gmail.com>
2023-07-17 21:51:33 +0300

    Updated dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,32 +3,22 @@
 
 /**
  * struct dog - Creates three members
+ * dog_t - Typedef for struct dog
  * @name: First member
  * @age: Second member
  * @owner: Third member
  *
  * Description: Dog's owner, dog's name and age
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-/**
- * dog_t - Typedef for struct dog
- */
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
