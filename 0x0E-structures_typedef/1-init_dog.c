f797dc795329fe9d0cf9e6ce4df98fc55cdcccf0
TimoEle <tinjokuria@gmail.com>
2023-07-17 21:14:16 +0300

    Function to initialize a struct type

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - Entry point
@@ -14,6 +15,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
