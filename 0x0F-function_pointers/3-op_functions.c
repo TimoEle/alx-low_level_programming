4eecb27134d08c68ab5a887ea7831f32c8e98e4f
TimoEle <tinjokuria@gmail.com>
2023-07-20 13:55:36 +0000

    Added a calculator program

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -52,7 +52,7 @@ int op_div(int a, int b)
 	if (a == 0 || b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a / b);
 }
@@ -70,7 +70,7 @@ int op_mod(int a, int b)
 	if (a == 0 || b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a % b);
 }
